77b793e8c543cb7c065ee9a5f3c152f95a651e85
Ryan Durrant <rddurrant@gmail.com>
2017-02-24 12:44:52 -0700

    almost complete, just need to fix css

--- js/controller/bookedCtrl.js
+++ js/controller/bookedCtrl.js
@@ -2,8 +2,25 @@ angular.module('devmtnTravel').controller('bookedCtrl', function($state, $scope,
   console.log($state);
 
   $scope.selectedObj = $state.params.id;
-
-  console.log('Ryan', $scope.selectedObj);
+  var idNum = $scope.selectedObj;
+  console.log('Ryan', $scope.selectedObj, idNum);
 
   //set up a function that loops through the mainSrv.travelInfo and checks to see if the id on $state.params = one of the id's in mainSrv.travelInfo
+
+  $scope.loopThrough = function(id){
+    for(var i = 0, len = mainSrv.travelInfo.length; i < len; i++){
+      if(mainSrv.travelInfo[i].id == id){
+        // console.log(mainSrv.travelInfo[i].image);
+        return mainSrv.travelInfo[i];
+      }
+    }
+  };
+
+  $scope.bookedDestination = $scope.loopThrough(idNum);
+  // console.log(bookedDestination);
+  // $scope.imageURL = $scope.bookedDestination.image;
+  // console.log($scope.imageURL);
+  // // $scope.styleObj = {
+  // //   "background": "url($scope.bookedDestination.image)"
+  // // };
 });
